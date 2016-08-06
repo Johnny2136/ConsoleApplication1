f49f8bf992cd10f0fdaa24a76490b4265ca950f2
Johnny Johnson <johnny2136@gmail.com>
2016-08-05 21:13:45 -0400

    FizzBuzz Logic

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,26 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+//#include "stdlib.h"
 
 
-int main()
-{
+
+using namespace std;
+
+int main(){
+	cout << "Hello FizzBuzz!\n";
+		
+	for (int i = 1; i < 101; i++) {
+		if (i % 3 == 0 && i % 5 == 0 )
+			cout << "FizzBuzz" << endl;
+		else if (i % 3 == 0)
+			cout << "Fizz" << endl;
+		else if (i % 5 == 0)
+			cout << "Buzz" << endl;
+			cout << i << endl;
+	}; 
+
+	system("pause");
     return 0;
 }
-
